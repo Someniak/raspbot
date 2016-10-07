95d580dcfaa31e51075cfb40f031f76da9186a8a
Darthvader <someniak@hotmail.com>
2016-10-07 16:11:10 +0200

    Some decent progress made

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-var net = require('net');
+var net = require('net'),
+    Connection = require('./../models/connection'),
+    parser =require('./../utils/parser'),
+    processor =require('../utils/processor'),
+    config = require('../config.json'),
+    publicIp = require('public-ip');
+
+
+
 var connections = [];
-var Connection = require('./../models/connection');
-var parser =require('./../utils/parser');
-var processor =require('../utils/processor');
-
-
 net.createServer(function (clientSocket) {
     //On connection push client to this list
 
@@ -13,7 +16,7 @@ net.createServer(function (clientSocket) {
 
     //On data received from client
     clientSocket.on('data', function (packet) {
-        console.log(`${connection.ip}:${connection.port} : Data has been received`);
+        console.log(`IN ${connection.ip}:${connection.port} : Data has been received`);
         var parsedData = parser.decode(packet);
         processor.execute(parsedData, send, broadcast).then(()=> {
             console.log(`${connection.port} : Data processed succesfully`);
@@ -22,28 +25,28 @@ net.createServer(function (clientSocket) {
 
     //On connection lost
     clientSocket.on('end', function () {
-        console.log(`${connection.ip}:${connection.port} : Connection has been lost`);
+        console.log(`IN ${connection.ip}:${connection.port} : Connection has been lost`);
         connections.splice(connections.indexOf(connection), 1);
     });
 
     var send = function(data){
-
+        console.log(`OUT ${connection.ip}:${connection.port} : Sending data`);
+        clientSocket.send(data);
     };
-    var broadcast = function(){
-        connections.forEach((client) => {
-            if (client === sender) return;
+    var broadcast = function(data){
+        console.log(`OUT ${connection.ip}:${connection.port} : Sending broadcast`);
+        connections.forEach((clientSocket) => {
+            if (client === connection) return;
+            clientSocket.write(data);
         })
     };
+}).listen(config.default_server_port);
 
-
-}).listen(5000);
-
-function broadcast(message, sender) {
-    connectedClients.forEach(function (client) {
-        // Don't want to send it to sender
-        if (client === sender) return;
-        clientSocket.write(message);
+var ipV4, ipV6;
+publicIp.v4()
+    .then((ip) => ipV4 = ip)
+    .then(() => publicIp.v6())
+    .then((ip) => ipV6 = ip)
+    .then(() => {
+        console.log(`Server is listening on ${ipV4}:${ipV6}`)
     });
-    // Log it to the server output too
-    process.stdout.write(message)
-}
