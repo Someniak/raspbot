3a330f45fd6d872ffb6309e0d8b737792a0f0e7e
Darthvader <someniak@hotmail.com>
2016-12-04 17:16:26 +0100

    Working my friends!

--- socket_server/crypto.js
+++ socket_server/crypto.js
@@ -3,11 +3,20 @@ var config = require('../config.json');
 var encryptor = require('simple-encryptor')(config.encryption_key);
 
 
-module.exports.decrypt = function(ciphertext){
-    return ciphertext;
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = config.encryption_key;
 
+module.exports.encrypt = function(text){
+    var cipher = crypto.createCipher(algorithm,password)
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
 };
 
-module.exports.encrypt = function(plaintext){
-    return plaintext;
+module.exports.decrypt = function(text){
+    var decipher = crypto.createDecipher(algorithm,password)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
 };
